87eea9a537fa2926cd46e2dbcabea0ef89ffcf8d
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-05-21 22:02:28 -0500

    actualizacion

--- admiTorneos/scripts/registarTorneo.js
+++ admiTorneos/scripts/registarTorneo.js
@@ -8,31 +8,37 @@ var sede=$('#sede');
 var fechaInicio=$('#fechaIni');
 var fechaFin=$('#fechaFin');
 
-$('#registrar').click(function(){
-    var datos = ('nombre='+nombre.val()+'&modalidad='+modalidad.val()+'&categoria='+categoria.val()+'&numEquipos='+numEquipos.val()+
-                '&sede='+sede.val()+'&fechaInicio='+fechaInicio.val()+'&fechaFin='+fechaFin.val());
-    alert(datos);
-    $.ajax({
-        type: 'get',
-        url: './php/crearTorneo.php',
-        data: datos,
-        success: function (respuesta) {
-            if (respuesta==1) {
-                alert('correcta');
-                
-                nombre.val('');
-                modalidad.val('');
-                categoria.val('');
-                numEquipos.val('');
-                sede.val('');
-                fechaIni.val('');
-                fechaFin.val('');
 
-            }else{
-                alert('incorrecta');
+    $('#registrar').click(function(){
+        
+        if(nombre.val()=="" || modalidad.val()=="" || categoria.val()=="" || numEquipos.val()=="" || sede.val()=="" || fechaInicio.val()=="" || fechaFin.val()==""){
+            alert("todos los campos son obligatorios");
+        }else{
+        var datos = ('nombre='+nombre.val()+'&modalidad='+modalidad.val()+'&categoria='+categoria.val()+'&numEquipos='+numEquipos.val()+
+                    '&sede='+sede.val()+'&fechaInicio='+fechaInicio.val()+'&fechaFin='+fechaFin.val());
+        alert(datos);
+        $.ajax({
+            type: 'get',
+            url: './php/crearTorneo.php',
+            data: datos,
+            success: function (respuesta) {
+                if (respuesta==1) {
+                    alert('correcta');
+                    
+                    nombre.val('');
+                    modalidad.val('');
+                    categoria.val('');
+                    numEquipos.val('');
+                    sede.val('');
+                    fechaIni.val('');
+                    fechaFin.val('');
+
+                }else{
+                    alert('incorrecta');
+                }
             }
-        }
-    });
-
-    });
+        });
+    
+      }  });
+    
 });
