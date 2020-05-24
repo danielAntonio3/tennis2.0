078284fdf70517a167a7d92b983d46b9db6489ff
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-05-23 19:09:24 -0500

    sinclonice el crear torneo,ver tornoe y crear usuarios

--- perfiles/js/perfil.js
+++ perfiles/js/perfil.js
@@ -1,18 +1,62 @@
 $(document).ready(function () {
     
     var registrarUser=$('#registrarUser');
-
+    var contenedor=$('#recargar');
     registrarUser.click(function(){ 
         alert("hola");
-
+        var espera=2000;
         $.ajax({
-            url: "../../",
-            success: function (response) {
-                
+            url: "../../crearUsuarios/crearUsuarios.html",
+            beforeSend:function(){
+                contenedor.text('Cargando...');
+            },
+            success:function(datos){
+                setTimeout(function(){
+                        contenedor.html(datos);
+                },espera);
             }
         });
     });
 
+    /*PARA QUE CUANDO EL USUARIO*/
+    var verTorneo=$('#verTorneo');
+    var contenedor=$('#recargar');
 
+    verTorneo.click(function(){ 
+        alert("hola");
+        var espera=2000;
+        $.ajax({
+            url: "../../admiTorneos/tablaTorneos.html",
+            beforeSend:function(){
+                contenedor.text('Cargando...');
+            },
+            success:function(datos){
+                setTimeout(function(){
+                        contenedor.html(datos);
+                },espera);
+            }
+        });
+    });
+
+     /*PARA QUE CUANDO EL admin quiere crear nuevo torneo*/
+     var crearTorneo=$('#crearTorneo');
+     var contenedor=$('#recargar');
+ 
+     crearTorneo.click(function(){ 
+         alert("hola");
+         var espera=2000;
+         $.ajax({
+             url: "../../admiTorneos/registrarTorneo.html",
+             beforeSend:function(){
+                 contenedor.text('Cargando...');
+             },
+             success:function(datos){
+                 setTimeout(function(){
+                         contenedor.html(datos);
+                 },espera);
+             }
+         });
+     });
+    
 
 });
