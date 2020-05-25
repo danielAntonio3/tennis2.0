017eeb65287f3f90ae061f7221b47f755047d697
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-05-25 12:01:35 -0500

    iniciar sesion listo solo falta checar que se va a pasar entre php para saber que usuario se esta ejecutando

---
+++ inicioSesion/js/iniciarSesion.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+        var email=$('#inputCorreo');
+        var contrasena=$('#inputPassword');
+        var boton=$('#iniciarSesion');
+
+
+        boton.click(function (e) { 
+            e.preventDefault();
+            if(email.val()=="" || contrasena.val()==""){
+                console.log("Todo los capos son obligatorios");
+            }else{
+                var datos=('correo='+email.val()+'&contrasena='+contrasena.val())
+                //console.log(datos);
+                $.ajax({
+                    type: "post",
+                    url: "../inicioSesion/php/iniciarSesion.php",
+                    data: datos,
+                    success: function (respuestas) {
+                        if (respuestas==1){
+                            console.log("eres administrador");
+                            window.location.replace('/tennis/tennis2.0/perfiles/perfil.html');
+                        }if(respuestas==2){
+                            console.log("eres monitor");
+                            window.location.replace('/tennis/tennis2.0/perfiles/perfil.html');
+                        }if(respuestas==3){
+                            console.log("eres federacion");
+                            window.location.replace('/tennis/tennis2.0/perfiles/perfil.html');
+                        }if(respuestas==4){
+                            console.log("eres jugador");
+                            window.location.replace('/tennis/tennis2.0/perfiles/perfil.html');
+                        }if(respuestas==0){
+                            console.log("lo sentimos no estar registrado");  
+                        }
+                    }
+                });
+            
+            }
+
+        });
+
+
+});
---
+++ inicioSesion/php/iniciarSesion.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+$correo=$_SESSION['correo']=$_GET['correo'];
+
+$usuario = 'root';
+$pass = "1234";
+
+  $mbd = new PDO('mysql:host=localhost;dbname=lobotennis', $usuario, $pass);
+
+  $correo=$_POST['correo'];
+  $contrasena=$_POST['contrasena'];
+   
+      $queryBusqueda ="select correo,contrasena,tipo_user from Usuario where correo='".$correo."'";
+      $stmt = $mbd->prepare($queryBusqueda);
+      $stmt->execute();
+
+      $row=$stmt->fetch(PDO::FETCH_BOTH);
+         $correoR=$row['correo'];
+         $contraR=$row['contrasena'];
+         $tipo_usuario=$row['tipo_user'];
+
+         if($correo ==$correoR  && $contrasena == $contraR){
+            echo "$tipo_usuario";
+         }else
+          {
+            echo"0";
+         }
+?>
