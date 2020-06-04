7e5cf9de365a89184929990ccf8a809a93945b87
danielAntonio3 <49245656+danielAntonio3@users.noreply.github.com>
2020-06-04 15:12:48 -0500

    mostar ranking

--- ranking/js/ranking.js
+++ ranking/js/ranking.js
@@ -8,10 +8,24 @@ $(document).ready(function () {
         $.ajax({
             url: "../ranking/php/cargaRanking.php",
             success: function (respuesta) {
-                
+                var js = JSON.parse(respuesta);
+                console.log(js);
+                var tamarreglo = js.length;
+                //console.log(tamarreglo);
+                for (x = 0; x < tamarreglo; x++) {
+                  var nom = js[x].nombre;
+                  var aplli= js[x].apellido;
+                  var ranki = js[x].ranking_atp;
+                  var fot = js[x].foto;
+                  var pais = js[x].modalidad_torneo;
 
 
+                }
             }
         });
     }
+
+
+    cargaRankig();
+
 });
